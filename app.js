324360c8468a44534d5507f330ed1999709247b0
Patrick Burke <pburke5@gmail.com>
2021-09-16 21:06:12 -0700

    Fixing public folder issue

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("/public"));
 app.use(helmet());
 
 app.get("/", function(req, res){
