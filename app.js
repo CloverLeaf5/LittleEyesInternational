e2c4f795a3547cdfede1a2aac9b7de542654f522
Patrick Burke <pburke5@gmail.com>
2021-09-17 23:08:26 -0700

    Add path

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(helmet.contentSecurityPolicy({
 				 directives:{
 				   defaultSrc:["'self'"],
