bc275080b224878d8e234a171536fd5171c9d9df
Patrick Burke <pburke5@gmail.com>
2021-09-17 18:11:33 -0700

    Updating public folder path

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(express.static(__dirname + '/public'));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(helmet.contentSecurityPolicy({
 				 directives:{
 				   defaultSrc:["'self'"],
