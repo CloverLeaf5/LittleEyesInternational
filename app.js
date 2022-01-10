5bdac136e7f6a079c4df1157c6e6a0b3527c77c1
Patrick Burke <pburke5@gmail.com>
2022-01-09 19:04:25 -0700

    Padding and Sizing

--- app.js
+++ app.js
@@ -17,15 +17,17 @@ app.use(express.static(__dirname + '/public'));
 //const path = require('path')
 //app.use('/static', express.static(path.join(__dirname, 'public')))
 //app.use(express.static(path.join(__dirname, 'public')))
+// Helmet sets security policies in HTTP responses
 app.use(helmet.contentSecurityPolicy({
 				 directives:{
 				   defaultSrc:["'self'"],
            imgSrc: ["'self'", '*'],
-				   scriptSrc:["'self'",'donorbox.org','maxcdn.bootstrapcdn.com','cdn.jsdelivr.net'],
+				   scriptSrc:["'self'",'donorbox.org','maxcdn.bootstrapcdn.com','cdn.jsdelivr.net','kit.fontawesome.com'],
 				   styleSrc:["'self'",'maxcdn.bootstrapcdn.com', "'unsafe-inline'", '*'],
            frameSrc:['donorbox.org'],
 				   fontSrc:["'self'",'*']}}));
 
+// Page gets
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -38,12 +40,13 @@ app.get("/donate", function(req, res) {
   res.render("donate");
 })
 
-
+// Post request to subscribe to Mailchimp mail list
 app.post("/subscribe", function(req, res) {
   const fName = req.body.firstName;
   const lName = req.body.lastName;
   const email = req.body.emailInput;
 
+	// Create the data that was pulled from the post body
   const dataToSend = {
     members: [
       {
@@ -57,6 +60,7 @@ app.post("/subscribe", function(req, res) {
     ]
   };
 
+	// Create the options for the axios HTTP post request
   const options = {
     method: "post",
     url: `${process.env.MAILCHIMP_API_URL}/lists/${process.env.MAILCHIMP_LIST_ID}`,
@@ -67,6 +71,7 @@ app.post("/subscribe", function(req, res) {
     data: dataToSend
   };
 
+	// Make the post request with axios
   axios(options)
   .then((response) => {
     console.log(response.data);
@@ -81,7 +86,7 @@ app.post("/subscribe", function(req, res) {
 });
 
 
-
+// Start the server
 app.listen(3000, '0.0.0.0', function(){
   console.log("Server started on port 3000.");
 });
