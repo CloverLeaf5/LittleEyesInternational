4fe8a1eb625524ba192d850aa0a7a518acb865b3
Patrick Burke <pburke5@gmail.com>
2024-07-20 17:02:23 -0400

    Add Blog page

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const axios = require("axios");
 const helmet = require("helmet");
+const AWS = require("aws-sdk");
+
+// GLOBAL TIME VARIABLE AND CACHE OF DB DATA
+let lastUpdateTime = 0;
+let lastDBData = {
+	title: "Under construction",
+	date: "July 10, 2024",
+	bodyArray: ["Check back soon!"],
+	photoURLs: []
+}
 
 
 const app = express();
@@ -44,9 +54,88 @@ app.get("/about", function(req, res) {
   res.render("about");
 })
 
-app.get("/blog", function(req, res) {
-  res.render("blog");
-})
+// Fetch data from DynamoDB and S3 prior to returning page
+app.get("/blog", async function(req, res) {
+	if ( lastUpdateTime > (Date.now() - (60*60*1000)) ) { // Updated within last hour
+		res.render("blog", {
+			title: lastDBData.title,
+			date: lastDBData.date,
+			bodyArray: lastDBData.bodyArray,
+			photoURLs: lastDBData.photoURLs
+		});
+		return;
+	}
+
+	// Has not updated in the last hour
+	AWS.config.update({ region: "us-east-1" });
+	// DynamoDB Query
+	const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+  const response = await ddb
+    .scan({
+      TableName: `${process.env.TABLE_NAME}`,
+			ProjectionExpression: "postID, title, displayDate, body, photoFolder, photoNames",
+			FilterExpression: "isActive = :a",
+		  ExpressionAttributeValues: {
+		    ":a": { BOOL: true },
+		  },
+
+    })
+    .promise().catch((error) => console.error(JSON.stringify(error, null, 2)));
+
+	const mostRecent = response.Items.reduce(
+		(prev, current) => {
+			return parseInt(prev.postID.N) > parseInt(current.postID.N) ? prev : current
+		}
+	);
+
+	console.log(mostRecent);
+
+	// Get data from DynamoDB Query
+	const title = mostRecent.title.S;
+	const date = mostRecent.displayDate.S;
+	const bodyArray = mostRecent.body.S.split("&&&&&");
+	let photoNames;
+	let photoFolder;
+	if (mostRecent.photoNames) {
+		photoNames = mostRecent.photoNames.SS;
+	} else {
+		photoNames = [];
+	}
+	if (mostRecent.photoFolder) {
+		photoFolder = mostRecent.photoFolder.S;
+	} else {
+		photoFolder = "";
+	}
+
+
+	// Get photo URLs from S3
+	let photoURLs = [];
+	if (photoNames.length > 0) {
+		const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+		for (let photoName of photoNames){
+			const params = {Bucket: `${process.env.S3_BUCKET}`, Key: `${photoFolder}/${photoName}`};
+	    const url = await s3.getSignedUrlPromise('getObject', params).catch((err) => console.error(err));
+			photoURLs.push(url);
+		}
+	}
+
+	// Update the cache
+	lastUpdateTime = Date.now();
+	lastDBData = {
+		title: title,
+		date: date,
+		bodyArray: bodyArray,
+		photoURLs: photoURLs
+	}
+
+	res.render("blog", {
+		title: title,
+		date: date,
+		bodyArray: bodyArray,
+		photoURLs: photoURLs
+	});
+});
+
 
 // Post request to subscribe to Mailchimp mail list
 app.post("/subscribe", function(req, res) {
@@ -91,6 +180,15 @@ app.post("/subscribe", function(req, res) {
     res.render("failure");
   });
 
+});
+
+
+// Post request to subscribe to Mailchimp mail list
+app.post("/resetDBTimer", function(req, res) {
+	const authCode = req.body.authCode;
+	if (authCode === process.env.DB_AUTH_CODE) {
+		lastUpdateTime = Date.now() - (60*60*1000);
+	}
 });
 
 
