44542c629287be132a2d65d06ddb23e92cf86441
Patrick Burke <pburke5@gmail.com>
2026-03-22 12:05:23 -0400

    Update nodemon install and remove newsletter

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const axios = require("axios");
 const helmet = require("helmet");
 const AWS = require("aws-sdk");
 
@@ -42,10 +41,6 @@ app.use(helmet.contentSecurityPolicy({
 app.get("/", function(req, res){
   res.render("home");
 });
-
-app.get("/signup", function(req, res) {
-  res.render("signup");
-})
 
 app.get("/donate", function(req, res) {
   res.render("donate");
@@ -135,51 +130,6 @@ app.get("/blog", async function(req, res) {
 	});
 });
 
-
-// Post request to subscribe to Mailchimp mail list
-app.post("/subscribe", function(req, res) {
-  const fName = req.body.firstName;
-  const lName = req.body.lastName;
-  const email = req.body.emailInput;
-
-	// Create the data that was pulled from the post body
-  const dataToSend = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: fName,
-          LNAME: lName
-        }
-      }
-    ]
-  };
-
-	// Create the options for the axios HTTP post request
-  const options = {
-    method: "post",
-    url: `${process.env.MAILCHIMP_API_URL}/lists/${process.env.MAILCHIMP_LIST_ID}`,
-    auth: {
-      username: "anystring",
-      password: process.env.MAILCHIMP_API_KEY
-    },
-    data: dataToSend
-  };
-
-	// Make the post request with axios
-  axios(options)
-  .then((response) => {
-    console.log(response.data);
-    console.log(response.status);
-    res.render("success");
-  })
-  .catch(error => {
-    console.log(error);
-    res.render("failure");
-  });
-
-});
 
 
 // Called by AWS if DynamoDB is updated.
